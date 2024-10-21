4705778984f8da808241a82925cd1f8538ed7ce2
Dharamveer <dveersingh000@gmail.com>
2024-10-22 03:13:41 +0530

    basic server setup for task app

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/v1", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  mongoose
+    .connect(process.env.MONGOOSE_URI_STRING, {})
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
