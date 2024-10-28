87433b32e31997e83a96679eceebd1be4908e7f9
Dharamveer <dveersingh000@gmail.com>
2024-10-29 02:56:12 +0530

    schema creation, routes, auth

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
+const userRouter = require("./routes/user");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/v1", indexRouter);
+app.use("/api/v1/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
