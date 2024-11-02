c3fe2071eb4d9a2ef5aab9dae7ca580ff3bc5c1c
Dharamveer <dveersingh000@gmail.com>
2024-11-02 21:34:48 +0530

    url update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(cors({
-  origin: "http://localhost:5173/", 
+  origin: "http://localhost:5173", 
   credentials: true
 }));
 app.use(bodyParser.json());
