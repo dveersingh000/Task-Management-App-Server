19e421742332d42e7b1aa7f6e2903602b40d59e9
Dharamveer <dveersingh000@gmail.com>
2024-11-02 19:35:27 +0530

    server link updated

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const indexRouter = require("./routes/index");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 const userRouter = require("./routes/user");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173/", 
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/v1", indexRouter);
 app.use("/api/v1/user", userRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
